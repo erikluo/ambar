b21aef526b673b81ff45648650d1d95a8788aff6
erikluo <whutluohui@qq.com>
2021-12-25 22:41:40 +0800

    node version

--- FrontEnd/compile.sh
+++ FrontEnd/compile.sh
@@ -5,6 +5,7 @@ source /usr/local/.nvm/nvm.sh
 echo "#####"
 cat /etc/hosts
 echo "***"
-nvm install v5.0.0
+nvm install v6.9.1
 npm install
-npm run deploy:prod
+npm run compile
+#npm run deploy:prod
